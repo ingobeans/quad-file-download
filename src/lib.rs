40c63711589eb24adfb02ee54a6bf6a34e702194
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-02-23 19:42:02 +0100

    format

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[cfg(target_arch = "wasm32")]
-use sapp_jsutils::{JsObject,JsObjectWeak};
+use sapp_jsutils::{JsObject, JsObjectWeak};
 
 #[no_mangle]
 extern "C" fn quad_file_download_crate_version() -> u32 {
@@ -15,23 +15,23 @@ extern "C" {
     fn quad_file_download(path: JsObjectWeak, bytes: JsObjectWeak);
 }
 
-pub fn download(path: &str, bytes: &[u8])->Result<(),std::io::Error> {
-	#[cfg(target_arch = "wasm32")]
-	{
-		unsafe {
-			let object = JsObject::buffer(bytes);
-			quad_file_download(JsObject::string(path).weak(),object.weak());
-			Ok(())
-		}
-	}
-	#[cfg(not(target_arch = "wasm32"))]
-	{
-		let dialog = rfd::FileDialog::new().set_file_name(path);
-		let path = dialog.save_file();
-		if let Some(path) = path {
-			std::fs::write(path, bytes)
-		} else {
-			Err(std::io::Error::other("File dialog was cancelled"))
-		}
-	}
+pub fn download(path: &str, bytes: &[u8]) -> Result<(), std::io::Error> {
+    #[cfg(target_arch = "wasm32")]
+    {
+        unsafe {
+            let object = JsObject::buffer(bytes);
+            quad_file_download(JsObject::string(path).weak(), object.weak());
+            Ok(())
+        }
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        let dialog = rfd::FileDialog::new().set_file_name(path);
+        let path = dialog.save_file();
+        if let Some(path) = path {
+            std::fs::write(path, bytes)
+        } else {
+            Err(std::io::Error::other("File dialog was cancelled"))
+        }
+    }
 }
